9526d6d46c840e07f58b8bbf02582cd74d0a470b
Jackson-Matthew <164759934+Jackson-Matthew@users.noreply.github.com>
2026-09-23 17:11:05 +0200

    Added create Project UI

--- BlastPro/BlastPro/Models/ViewModels/CreateProjectViewModel.cs
+++ BlastPro/BlastPro/Models/ViewModels/CreateProjectViewModel.cs
@@ -1,1 +1,21 @@
+using System.ComponentModel.DataAnnotations;
 
+namespace BlastPro.Mvc.Models.ViewModels.Projects
+{
+    public class CreateProjectViewModel
+    {
+        [Required(ErrorMessage = "Project name is required.")]
+        [StringLength(100)]
+        [Display(Name = "Project Name")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Site location is required.")]
+        [StringLength(150)]
+        [Display(Name = "Site Location")]
+        public string SiteLocation { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please select a blast type.")]
+        [Display(Name = "Blast Type")]
+        public string BlastType { get; set; } = string.Empty;
+    }
+}
