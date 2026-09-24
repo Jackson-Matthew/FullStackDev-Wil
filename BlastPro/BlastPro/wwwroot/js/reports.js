fe53ddfc919e565eb5b0d3b0c01f18e03b6b6685
FarquharNicholas <ST10439055@imconnect.edu.za>
2026-09-24 21:57:33 +0200

    Add report UI

---
+++ BlastPro/BlastPro/wwwroot/js/reports.js
@@ -0,0 +1,17 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+
+    const printBtn =
+        document.getElementById("printReportBtn");
+
+    if (!printBtn) {
+        return;
+    }
+
+    printBtn.addEventListener(
+        "click",
+        function () {
+            window.print();
+        }
+    );
+
+});
